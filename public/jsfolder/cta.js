631438630abe7504de437b00c85e889d47eb0840
programmer Kartier <progrmmerKartier@gmail.com>
2023-10-01 22:23:15 +0200

    majorpush

---
+++ public/jsfolder/cta.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.querySelector("#newsForm");
+  
+    form.addEventListener("submit", (e) => {
+      const inpField = document.querySelector("#emailInp");
+      const errMessage = document.querySelector("#errMess");
+  
+      if (inpField.value.length === 0) {
+        e.preventDefault();
+        errMessage.textContent = "Situation Bruvvvisto";
+      } else {
+        console.log("Khuzani");
+      }
+    });
+  });
+  
