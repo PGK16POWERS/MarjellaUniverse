aba3e411d3b3576631575ecbd1470d3d9dea7936
programmer Kartier <progrmmerKartier@gmail.com>
2023-10-02 09:03:33 +0200

    server

--- node.js
+++ node.js
@@ -2,10 +2,12 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const cookieParser = require("cookie-parser");
+const ejs = require("ejs");
 
-
+app.set("view engine","ejs")
 app.use(cookieParser());
 app.use(express.static("public"));
+app.use(express.static("views"))
 
 
 app.get("/", (req,res)=> {
@@ -13,6 +15,9 @@ app.get("/", (req,res)=> {
     res.sendFile(path.join(__dirname, "public","index.html"));
 });
 
+app.get("/qoute", (req,res)=> {
+    res.render("qoutePage.ejs")
+})
 
 
 app.listen(6500, ()=> {
